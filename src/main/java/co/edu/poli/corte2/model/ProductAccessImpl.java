dc9ac714f5b675522853787bab67219824113f21
jhonFBernalPoli <jfrbernalc@poligran.edu.co>
2025-04-30 11:26:41 -0500

    feat: Creación de formulario para guardar Productos

--- src/main/java/co/edu/poli/corte2/model/ProductAccessImpl.java
+++ src/main/java/co/edu/poli/corte2/model/ProductAccessImpl.java
@@ -3,19 +3,8 @@ package co.edu.poli.corte2.model;
 public class ProductAccessImpl implements ProductAccess {
 
     @Override
-    public String viewProductDetail(Product product) {
-            
-        if (product == null) {
-            return "Producto no encontrado";
-        }
-        
-        StringBuilder details = new StringBuilder();
-        details.append("ID: ").append(product.getId()).append("\n");
-        details.append("Nombre: ").append(product.getName()).append("\n");
-        details.append("Descripción: ").append(product.getDescription()).append("\n");
-        details.append("Precio: $").append(String.format("%.2f", product.getPrice()));
-        
-        return details.toString();
+    public Product viewProductDetail(Product product) {     
+        return product;
     }
 
 }
